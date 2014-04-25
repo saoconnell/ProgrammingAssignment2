1926aefa3dd2a02ac99622ad70d45a5d71196e9d
Stephen <sao@saoconnell.com>
2014-04-25 14:57:43 -0700

    update contains the code to meet the assignment requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## code location: https://github.com/saoconnell/ProgrammingAssignment2.git
+## 
+## makeCacheMatrix:
+##    Create a special that sovles the inverse of a matrix, 
+##      caching the result
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## 
+## cacheSolve:
+##    Invert a special matrix created with the makeCacheMatrix,
+##      if it a new reference, it will calculate the inverse, if it has
+##      been previously calculated it will use the cached value.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
+
+
+##  NOT RUN:
+
+## TEST FUNCTION
+## create an invertible matrix
+#hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+#h8 <- hilbert(8)
+
+## make a special matrix with the makeCacheMatrix function
+# iX <- makeCacheMatrix(h8)
+
+## use the special matrix form of invertible matrix, and calculate
+##    The inverse, use the cached value if presviously calculated and 
+##    the matrix has not changed
+# system.time(cacheSolve(iX))
+
